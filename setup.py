08d84eb5ebaa4ff4fd8fba0ce11c5fd132ee8aa4
Orbnauticus <ryan.marquardt@gmail.com>
2010-11-28 16:28:51 +0900

    Adding package data

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os
+
+setup(name='winebottles', version='0.2.0',
+	author='Ryan Marquardt',
+	author_email='ryan.marquardt@gmail.com',
+	description='WineBottles',
+	url='http://orbnauticus.github.org/winebottles',
+	license='Simplified BSD License',
+	scripts=['bottle'],
+)
